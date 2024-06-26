50d953782dbfaaa7f0b5c25eb6116410d30e16f1
galexander722 <96560802+galexander722@users.noreply.github.com>
2024-06-25 23:51:37 -0500

    Update Function_Get-ODSystemInfo.ps1
    Added child properties to the main property which makes it even better

--- Function_Get-ODSystemInfo.ps1
+++ Function_Get-ODSystemInfo.ps1
@@ -70,7 +70,7 @@ function Get-ODSystemInfo {
                 if($ShowProgress) { Write-Progress -Activity "Working on $computer" -CurrentOperation "Operating System" -PercentComplete $current_complete}
                 $os = Get-WmiObject -Class Win32_operatingsystem -ComputerName $computer -ErrorAction Stop
             } catch{
-                Write-warning "Unable to connect to $computername's WMI. Please Verify the device is powered on or if the device name is valid. Logged into Error Log in C:\temp"
+                Write-warning "Unable to connect to $computer's WMI. Please Verify the device is powered on or if the device name is valid. Logged into Error Log in C:\temp"
                 $ConnectivityTest=$False
                 if($ShowProgress) { Write-Progress -Activity "Failed on $computer" -CurrentOperation "Failed to Connect" -Completed
                 $computer | Out-File $ErroLogFilePath -Append
@@ -80,18 +80,47 @@ function Get-ODSystemInfo {
                 $cs = Get-WmiObject -Class Win32_computersystem -ComputerName $computer
                 
                     if($ShowProgress) { Write-Progress -Activity "Working on $computer" -CurrentOperation "Network Adapter" -PercentComplete $current_complete}
-                $nt = Get-WmiObject -Class Win32_NetworkAdapter -ComputerName $computer
+                
                 
                     if($ShowProgress) { Write-Progress -Activity "Working on $computer" -CurrentOperation "Creating Object along with it's properties" -PercentComplete $current_complete}
-                $props = [ordered]@{
-                        'computername' = $computer;
-                        'Users' = $os.NumberOfUsers;
-                        'OS status'= $os.Status;
-                        'Macs'= $nt.macaddress;
-                        'Device Make' = $cs.manufacturer
-                        }
+                    $props = [ordered]@{
+                            'computername' = $computer;
+                            'Users' = $os.NumberOfUsers;
+                            'OS status'= $os.Status;
+                            'Macs'= $nt.macaddress;
+                            'Device Make' = $cs.manufacturer
+                            }
                 $obj = New-Object -TypeName psobject -Property $props
                 $obj.PSObject.TypeNames.Insert(0,'OD.Systeminfo')
+
+                write-verbose = "Getting local disk information"
+                $drives = Get-WmiObject -Class win32_logicaldisk -filter "DriveType=3" -ComputerName $computer
+                foreach($drive in $drives){
+                    $driveprops = [ordered] @{
+                                             'Drive'=$drive.deviceid;
+                                             'FreeSpace'=$drive.freespace;
+                                             'Size'=$drive.size;
+                                                }
+                    $driveobj = new-object -TypeName psobject -Property $driveprops
+                    $children += $driveobj
+                }
+                Write-Verbose = "Applying the child properties for the local disk to the object"
+                $obj | Add-Member -MemberType NoteProperty -Name 'LocalDrives' -value $children
+                $children = @()
+                Write-Verbose = "Grabbing network adapter via wmi"
+               $nics = Get-WmiObject -Class Win32_NetworkAdapter -ComputerName $computer -Filter "physicaladapter=$true"
+                foreach($nic in $nics){
+                    $nicprops = [ordered] @{
+                                             'MAC'=$nic.MACAddress;
+                                             'Device ID'=$nic.DeviceID;
+                                             'Speed'= $nic.Speed;
+                                             'AdapterType'=$nic.AdapterType
+                                                }
+                    $nicobj = new-object -TypeName psobject -Property $nicprops
+                    $children += $nicobj
+                }
+                Write-Verbose = "Applying the child properties of the network adapters to the object"
+                $obj | Add-Member -MemberType NoteProperty -Name 'NetworkAdapters' -value $children
                 write-output $obj
                     if($ShowProgress) { Write-Progress -Activity "Working on $computer" -CurrentOperation "Writing object and its properties to output" -PercentComplete $current_complete}
                 
@@ -102,3 +131,4 @@ function Get-ODSystemInfo {
     if($ShowProgress) { Write-Progress -Activity "Done on $computer" -CurrentOperation "Done" -Completed }
    }
 }
+
